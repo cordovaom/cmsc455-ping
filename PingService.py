cadd759ec3970be2ca702aefe2b59866408d070c
cordovaom <cordovaom@mymail.vcu.edu>
2020-09-23 02:56:45 -0400

    creating files
    creating ping app file and run.sh that uns the ping app.

---
+++ PingService.py
@@ -0,0 +1,39 @@
+import os
+import time
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_httpauth import HTTPDigestAuth
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret key here'
+
+auth = HTTPDigestAuth()
+
+users={"vcu": "ram"}
+
+@auth.get_password
+def get_password(username):
+    if username in users:
+        return users.get(username)
+    return None
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'message': 'Page Not Found'}), 404
+    
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({'message': 'Servier Error'}), 500
+
+@app.route('/user', methods=['GET'])
+@auth.login_required
+def ping():
+    startTime = time.time()
+    http GET http://
+    return jsonify({'message': 'Server was pinged, ' + str((time.time() - startTime)*1000)}), 200
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=PingService.py
+flask run
